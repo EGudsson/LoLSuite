5c7e321c609a704ea877458265f249ec4d44bf3e
EGudsson <elias.gudsson@gmail.com>
2026-06-08 23:11:23 +0200

    delete comments

--- resource.h
+++ resource.h
@@ -1,12 +1,7 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by LoLSuite.rc
-//
 #define IDS_APP_TITLE                   100
 #define IDM_EXIT                        101
 #define IDI_APP_ICON                    102
-// Next default values for new objects
-// 
+
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
